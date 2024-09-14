d3e3b75dfd4b3ebbf2d53fa791c7c5802d16dfe4
August Saint Freytag <me@augustfreytag.com>
2024-09-14 20:44:46 +0200

    Introduce condition use for shovel on campfire use

--- src/main/java/net/qxeii/hardcore_torches/mixinlogic/CampfireBlockMixinLogic.java
+++ src/main/java/net/qxeii/hardcore_torches/mixinlogic/CampfireBlockMixinLogic.java
@@ -1,12 +1,20 @@
 package net.qxeii.hardcore_torches.mixinlogic;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.CampfireBlock;
 import net.minecraft.block.entity.CampfireBlockEntity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
@@ -14,6 +22,7 @@ import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -73,6 +82,7 @@ public interface CampfireBlockMixinLogic {
 			}
 
 			CampfireBlockEntityMixinLogic.extinguish(world, pos, state);
+			useShovelItemStack(stack, player, hand);
 
 			if (world.isClient) {
 				player.swingHand(hand);
@@ -108,9 +118,51 @@ public interface CampfireBlockMixinLogic {
 		return ActionResult.SUCCESS;
 	}
 
+	private void useShovelItemStack(ItemStack stack, PlayerEntity player, Hand itemHand) {
+		stack.damage(1, player, forwardedPlayer -> {
+			breakShovelItemStack(player, stack, itemHand);
+		});
+	}
+
+	private void breakShovelItemStack(PlayerEntity player, ItemStack stack, Hand itemHand) {
+		var equipmentSlot = itemHand == Hand.MAIN_HAND ? EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND;
+		var itemSlot = player.getInventory().getSlotWithStack(stack);
+
+		player.sendEquipmentBreakStatus(equipmentSlot);
+		
+		if (!FabricLoader.getInstance().isModLoaded("ruined_equipment")) {
+			// If equipment mod is not loaded, item can be removed directly.
+			
+			player.getInventory().removeStack(itemSlot);
+			return;
+		}
+
+		var ruinedShovelItemId = shovelItemMap().get(stack.getItem().getTranslationKey());
+		var ruinedShovelItem = Registries.ITEM.get(ruinedShovelItemId);
+
+		if (ruinedShovelItem == Items.AIR) {
+			return;
+		}
+
+		var ruinedShovelStack = new ItemStack(ruinedShovelItem, 1);
+		player.getInventory().setStack(itemSlot, ruinedShovelStack);
+	}
+
 	private void displayFuelMessage(PlayerEntity player, int fuel) {
 		var fuelTimeMessage = WorldUtils.formattedFuelText(fuel);
 		player.sendMessage(fuelTimeMessage, true);
+	}
+
+	private Map<String, Identifier> shovelItemMap() {
+		return new HashMap<String, Identifier>(
+			Map.of(
+				"minecraft:wooden_shovel", new Identifier("ruined_equipment", "ruined_wooden_shovel"),
+				"minecraft:stone_shovel", new Identifier("ruined_equipment", "ruined_stone_shovel"),
+				"minecraft:iron_shovel", new Identifier("ruined_equipment", "ruined_iron_shovel"),
+				"minecraft:golden_shovel", new Identifier("ruined_equipment", "ruined_golden_shovel"),
+				"minecraft:diamond_shovel", new Identifier("ruined_equipment", "ruined_diamond_shovel")
+			)
+		);
 	}
 
 }
